07a7e22d7848d69aad1fafedaebd0d1178d15696
Shourya Bansal <shouryab2001@gmail.com>
2021-10-08 14:26:18 +0530

    second commit : add reducer, action and store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducer'
+
+const store  = createStore(movies);
+
+
+console.log('store',store);
+console.log('Before STATE',store.getState());
+
+store.dispatch({
+  type: 'ADD_MOVIES',
+  movies: [{name: 'Superman'}]
+});
+
+console.log('After STATE',store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
